73ff33cfe15722be37500676c5b08c73ffd68ea2
Joel Ostos <joelostoscastro@gmail.com>
2025-03-08 22:52:56 -0500

    Ready, Set, GO!

---
+++ queue.h
@@ -0,0 +1,51 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+
+#define INIT_SIZE 	10
+#define GROW_FACTOR 	2
+
+typedef struct Queue Queue;
+
+struct Queue {
+  size_t cap;
+  size_t used;
+  int idx;
+};
+
+#define queue_cast(x) ((Queue*) x - 1)
+#define queue_push(x, v) !(x) ?  x = queue_init(x, sizeof(*x)), queue_put(x, v) : queue_put(x,v) 
+#define queue_put(x, v) check(queue_cast(x)) ? (x)[queue_cast(x)->used] = v, queue_cast(x)->used++ : (queue_grow(x, sizeof(x[0])), (x)[queue_cast(x)->used] = v, queue_cast(x)->used++)
+#define queue_pop(x)   !(x)  ? printf("Unitialized queue\n"): queue_delete(x) 
+#define queue_delete(x)  queue_cast(x)->idx == queue_cast(x)->used ? (printf("Empty queue\n"), -1) :  (queue_cast(x)->idx++, (x)[queue_cast(x)->idx-1])
+
+bool check(Queue* x) 
+{
+  return !(x->used == (x->cap-1));
+}
+
+void* queue_init(void* x, size_t elem_size) 
+{
+  void* b = malloc(INIT_SIZE * elem_size + sizeof(Queue));
+  ((Queue*)b)->cap = INIT_SIZE;
+  ((Queue*)b)->used = 0;
+  ((Queue*)b)->idx = 0;
+  b = (Queue*) b + sizeof(Queue);
+  assert(b);
+  return b;
+}
+
+void* queue_grow(void* x, size_t elem_size) 
+{
+  void* b = realloc(queue_cast(x), ((Queue*)b)->cap * GROW_FACTOR * elem_size + sizeof(Queue));
+  ((Queue*)b)->cap = INIT_SIZE * GROW_FACTOR;
+  b = (Queue*) b + sizeof(Queue);
+  assert(b);
+  return b;
+}
+
+#endif
---
+++ stack.h
@@ -0,0 +1,49 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+
+#define INIT_SIZE 	10
+#define GROW_FACTOR 	2
+
+typedef struct Stack Stack;
+
+struct Stack {
+  size_t cap;
+  size_t used;
+};
+
+#define stack_cast(x) ((Stack*) x - 1)
+#define stack_push(x, v) !(x) ?  x = stack_init(x, sizeof(*x)), stack_put(x, v) : stack_put(x,v) 
+#define stack_put(x, v) stack_check(stack_cast(x)) ? (x)[stack_cast(x)->used] = v, stack_cast(x)->used++ : (stack_grow(x, sizeof(x[0])), (x)[stack_cast(x)->used] = v, stack_cast(x)->used++)
+#define stack_pop(x)   !(x)  ? printf("Unitialized stack\n"): stack_delete(x) 
+#define stack_delete(x)  stack_cast(x)->used == 0 ? printf("Empty stack\n") : (stack_cast(x)->used--, (x)[stack_cast(x)->used])
+
+bool stack_check(Stack* x) 
+{
+  return !(x->used == (x->cap-1));
+}
+
+void* stack_init(void* x, size_t elem_size) 
+{
+  void* b = malloc(INIT_SIZE * elem_size + sizeof(Stack));
+  ((Stack*)b)->cap = INIT_SIZE;
+  ((Stack*)b)->used = 0;
+  b = (Stack*) b + sizeof(Stack);
+  assert(b);
+  return b;
+}
+
+void* stack_grow(void* x, size_t elem_size) 
+{
+  void* b = realloc(stack_cast(x), ((Stack*)b)->cap * GROW_FACTOR * elem_size + sizeof(Stack));
+  ((Stack*)b)->cap = INIT_SIZE * GROW_FACTOR;
+  b = (Stack*) b + sizeof(Stack);
+  assert(b);
+  return b;
+}
+
+#endif
